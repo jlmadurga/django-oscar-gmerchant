c1fbc6a1664f9503885f83bce086e3c99513346a
Juan Madurga <jlmadurga@gmail.com>
2015-03-16 12:52:49 +0100

    tam configuration

--- gmerchant/client/conf.py
+++ gmerchant/client/conf.py
@@ -2,8 +2,8 @@ from django.contrib.sites.models import Site
 
 MAX_PAGE_SIZE = 50
 BATCH_SIZE = 50
-BRAND = "Example Brand"
-PREFIX = "example#"
+BRAND = "Trajesamedida.com"
+PREFIX = "trajesamedida#"
 PROTOCOL = "http://"
 try:
     SITE = Site.objects.first().domain
